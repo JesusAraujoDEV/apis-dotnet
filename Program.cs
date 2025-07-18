0d26cc743d7038f83f9f3b33dbc55d25ba600a19
Jesús Araujo <j2a0a0a5@gmail.com>
2025-07-18 09:46:45 -0400

    Clase 11: Add Swagger support and update project dependencies; remove unused minimal API files

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,18 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseWelcomePage();
 
 app.MapControllers();
 
